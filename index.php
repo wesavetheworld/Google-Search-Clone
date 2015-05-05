f850ddc71c94782095d6e8d240a91c6a3db299dc
Saqib Razzaq <saqib@clipbucket.com>
2015-05-05 19:51:33 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<title>Google</title>
+
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+<style type="text/css">
+	#mainsearch {
+		border-radius: 0px;
+	}
+	.btn-default {
+		border-radius: 0px;
+	}
+	.logo {
+		margin-left: 550px;
+		margin-bottom: 30px;
+	}
+	.se {
+		margin-left: 370px;
+	}
+	.bt {
+		margin-left: 560px;
+	}
+	.hey {
+		margin-left: 690px;
+		margin-top: -34px;
+	}
+	.main {
+		margin-top: 150px;
+	}
+</style>
+<div class="main">
+<img class="logo" src="download.png">
+
+<form action="search.php" method="GET" name="firstone">
+ 	<div class="col-sm-6">
+<input type="text" name="q" class="form-control se" id="mainsearch" >
+<br/>
+
+<button type="submit" class="btn btn-default bt" value="q">Google Search</button>
+<button type="" class="btn btn-default hey">I'm Feeling Lucky</button>
+</div>
+
+</form>
+
+</div>
